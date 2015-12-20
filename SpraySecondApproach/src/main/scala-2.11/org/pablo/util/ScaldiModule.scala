dd532c66440be5eb4f7ab88c0bc3d799d5b0bb32
PabloZed <pablo.gonzalez@bitmonlab.com>
2015-12-20 19:54:06 +0100

    .gitIgnore

--- SpraySecondApproach/src/main/scala-2.11/org/pablo/util/ScaldiModule.scala
+++ SpraySecondApproach/src/main/scala-2.11/org/pablo/util/ScaldiModule.scala
@@ -3,7 +3,9 @@ package org.pablo.util
 import scaldi.Module
 import org.pablo.service.PersonServiceInterface
 import org.pablo.service.PersonService
+import org.pablo.repository.PersonDao
 
 class ScaldiModule extends Module {
   bind[PersonServiceInterface] to new PersonService
+ 
 }
